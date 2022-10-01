741fe8fb763cf17c73357f1e480ebb9258084e9f
Trevor Rice <42627940+trev-dev0@users.noreply.github.com>
2022-09-30 22:25:21 -0400

    Create guards in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,31 @@
 <script>
-  import "../app.css";
+	import '../app.css';
+	import pocketbase from '$lib/pocketbase';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	const isLoggedIn = pocketbase.authStore.isValid;
+	const currentPath = $page.url.pathname;
+
+	/* 
+    if the user is logged in and they are trying
+    to go to /login or /register, redirect them
+    to the home page
+  */
+	onMount(() => {
+		if (isLoggedIn && (currentPath == '/login' || currentPath == '/signup')) {
+			goto('/');
+		} else if (!isLoggedIn && currentPath == '/') {
+			/* 
+        if the user is not logged in and they try to acces
+        the home page, redirect them to login
+      */
+			goto('/login');
+		}
+	});
 </script>
 
 <div class="min-h-screen bg-gray-50">
-<slot />
-
+	<slot />
 </div>
