38ab503c66d45b227a6311dc971053f34280f636
Trevor Rice <42627940+trev-dev0@users.noreply.github.com>
2022-09-30 22:24:53 -0400

    Create logout button on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import pocketbase from '$lib/pocketbase';
+	const logout = async () => {
+		await pocketbase.authStore.clear();
+		goto('/login');
+	};
+</script>
+
+<button class="bg-blue-500 rounded-xl p-3" on:click={logout}>Logout</button>
